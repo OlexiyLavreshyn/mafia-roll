ab6afc95abeb6c7b8f97e304b70b19ecec6d152f
OleksiyLavreshyn <alexxxx44442@gmail.com>
2024-12-31 20:05:30 +0200

    HashRouter instead of BrowerRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import RolesDisplay from './components/RolesDisplay';
 
